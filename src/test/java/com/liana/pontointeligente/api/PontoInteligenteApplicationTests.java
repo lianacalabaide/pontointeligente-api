e58745488eb385cc1c1b52687e38ef1decfab13a
lianacalabaide <lianusca@gmail.com>
2021-04-26 10:31:01 -0300

    mensagem inicial

---
+++ src/test/java/com/liana/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liana.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
